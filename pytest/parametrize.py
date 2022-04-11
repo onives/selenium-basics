dceff74326c9d000144bcf6fd9bbc55255f59341
Olive Nives Nameere <olivenives63@gmail.com>
2022-04-11 12:09:41 +0300

    parametrize

---
+++ pytest/parametrize.py
@@ -0,0 +1,16 @@
+# data driven testing
+import pytest
+
+products = [
+    (2, 3, 6),
+    (1, 99, 99),
+    (0, 99, 0),
+    (3, -4, -12),
+    (-5, -5, 25),
+    (2.5, 6.7, 16.75)
+]
+
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+    assert a * b == product
